c6948d4d9426f3bfa31d99215ea62dfee6992f3f
abidgulzar03-dev <abidgulzar03@gmail.com>
2026-04-24 21:54:04 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+let orderButton = document.querySelector(".rightbox");
+
+orderButton.addEventListener("click", function () {
+    orderButton.innerText = "Booking...";
+    orderButton.style.backgroundColor = "white";
+    orderButton.style.color = "black";
+    
+});
+
+
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))?.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+
+const toggleBtn = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+toggleBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+});
+
